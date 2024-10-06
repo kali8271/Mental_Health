4bb65e4e5bfc3bd3ee2038308d65e72bd6c22a78
KALIMULLAH ANSARI <akalimullah900@gmail.com>
2024-10-06 17:12:37 +0530

    updaed

--- Mental_Health_Awareness_Platform/admin_Panel/views.py
+++ Mental_Health_Awareness_Platform/admin_Panel/views.py
@@ -1,11 +1,13 @@
-# admin/views.py
-
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from accounts.models import UserProfile
+from .models import Professional
 
 @login_required
 def dashboard(request):
+    user_profile = UserProfile.objects.get(username=request.user.username)  # Get the user's profile
+    professional = Professional.objects.all()
     if request.user.is_superuser:
-        return render(request, 'admin/dashboard.html')  # Admin dashboard
+        return render(request, 'admin/dashboard.html', {'user_profile': user_profile,'professional ' : professional})  # Admin dashboard
     else:
-        return render(request, 'admin/user_dashboard.html')  # User dashboard
+        return render(request, 'admin/user_dashboard.html', {'user_profile': user_profile, 'professional' : professional})  # User dashboard
--- Mental_Health_Awareness_Platform/therapy_sessions/models.py
+++ Mental_Health_Awareness_Platform/therapy_sessions/models.py
@@ -9,6 +9,7 @@ class TherapySession(models.Model):
     session_duration = models.IntegerField(help_text="Duration in minutes")
     booked = models.BooleanField(default=False)
     created_at = models.DateTimeField(default=timezone.now)
+    price = models.DecimalField(max_digits=10, decimal_places=2,default=50.00)
 
     def __str__(self):
         return f"Session with {self.client.username} on {self.session_date}"
