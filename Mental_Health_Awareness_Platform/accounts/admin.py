58da5407197362cff910ba2ab8642ef708bc84fc
KALIMULLAH ANSARI <akalimullah900@gmail.com>
2024-10-08 21:45:42 +0530

    updated

--- Mental_Health_Awareness_Platform/accounts/admin.py
+++ Mental_Health_Awareness_Platform/accounts/admin.py
@@ -2,13 +2,33 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import UserProfile
 from django.contrib import admin
+from django.contrib import admin
+from .models import  UserProfile
+from experts.models import Expert
+
 
 admin.site.site_header = "Mental Health Awareness Platform Admin"
 admin.site.site_title = "Admin Panel"
 admin.site.index_title = "Welcome to the Admin Panel"
 
-from django.contrib import admin
-from .models import UserProfile
+  # Import your models
+
+# Customize the admin display for Expert
+# class ExpertAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'specialization', 'years_of_experience')
+#     search_fields = ('name', 'specialization')
+#     list_filter = ('years_of_experience',)
+
+# # Customize the admin display for UserProfile
+# class UserProfileAdmin(admin.ModelAdmin):
+#     list_display = ('user', 'photo', 'created_at')
+#     search_fields = ('user__username',)
+#     list_filter = ('created_at',)
+
+# # Register your models to appear in the admin interface
+# admin.site.register(Expert, ExpertAdmin)
+# admin.site.register(UserProfile, UserProfileAdmin)
+
 
 admin.site.register(UserProfile, UserAdmin)
 
