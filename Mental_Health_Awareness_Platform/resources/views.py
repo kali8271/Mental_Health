e683d7747ab3139dfd5db7eeb41e342abe469873
KALIMULLAH ANSARI <akalimullah900@gmail.com>
2024-10-29 14:00:18 +0530

    updated

--- Mental_Health_Awareness_Platform/resources/views.py
+++ Mental_Health_Awareness_Platform/resources/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Resource
 
 def resource_list(request):
     resources = Resource.objects.all().order_by('-created_at')
     return render(request, 'resources/resource_list.html', {'resources': resources})
 
+@login_required
 def resource_detail(request, resource_id):
     resource = get_object_or_404(Resource, id=resource_id)
     return render(request, 'resources/resource_detail.html', {'resource': resource})
